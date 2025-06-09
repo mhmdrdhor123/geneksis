19cb0d2d496d88589a60fd5cd0ba9341d059d0f9
mhmdrdhor123 <mhmdrdhor123@gmail.com>
2025-06-09 12:06:33 +0700

    Update script.js

--- admin/script.js
+++ admin/script.js
@@ -2,7 +2,6 @@ function login() {
   const user = document.getElementById("username").value;
   const pass = document.getElementById("password").value;
 
-  // Username dan password bisa lo ganti sendiri
   if (user === "admin" && pass === "geneksis123") {
     localStorage.setItem("login", "true");
     window.location.href = "dashboard.html";
